e6d015174f4afcaed83a4ea0479babf0b1ac4ce4
khaingmyozaw <kmyozaw.dev@gmail.com>
2024-06-27 22:21:12 +0630

    like button click action

--- resources/views/components/best-seller.blade.php
+++ resources/views/components/best-seller.blade.php
@@ -2,7 +2,7 @@
     <div class="sm:w-4/12">
         <img src="{{ asset($image) }}" alt="promotion" class="size-44 rounded-md -mt-12 object-cover mx-auto shadow-3xl border border-pre-extra-gree sm:h-60 md:h-72 md:w-64 bs-img">
     </div>
-    <div class="p-2 md-p-0 sm:w-8/12 bs-content flex sm:block flex-col items-center">
+    <div class="p-2 md-p-0 sm:w-8/12 bs-content flex sm:block flex-col items-centera">
         <x-title-component title="{{ $title }}" class="text-white"/>
         <p class="text-white text-center mt-5 md:text-start md:w-3/4">
             {{ $content }}
